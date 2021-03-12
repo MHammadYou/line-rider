e07467a2052a8f46af4e8fb4c1a0416f00a2929f
MHammadYou <MHammadY2.0@gmail.com>
2021-03-12 22:54:14 +0500

    Added LineUpdate Script

---
+++ Assets/Scripts/Line.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Line : MonoBehaviour
+{
+    public LineRenderer lineRenderer;
+    public EdgeCollider2D edgeCol;
+
+    private List<Vector2> _points;
+
+    public void UpdateLine(Vector2 mousePos)
+    {
+        if (_points == null)
+        {
+            _points = new List<Vector2>();
+            SetPoint(mousePos);
+            return;
+        }
+        
+        if (Vector2.Distance(_points.Last(), mousePos) > .1f)
+        {
+            SetPoint(mousePos);
+        }
+        
+    }
+    void SetPoint(Vector2 point)
+    {
+        _points.Add(point);
+
+        lineRenderer.positionCount = _points.Count;
+        lineRenderer.SetPosition(_points.Count - 1, point);
+
+        if (_points.Count > 1)
+        {
+            edgeCol.points = _points.ToArray();
+        }
+    } 
+}
